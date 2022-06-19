be5e1af074f968b7bc74e25ba31fd7cda23f933d
elfsvet <stepanmatysik@gmail.com>
2022-06-19 18:43:15 -0400

    deleting the database in heroku

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log(`Now listening on the port: 🌎 http://localhost:${PORT} 🌎`))
     });
